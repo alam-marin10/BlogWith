f5c9a2504f71f68a3b26afa3640c821e159872c0
alam-marin10 <alammarin10@gmail.com>
2025-03-11 03:10:11 +0600

    add new sections and create a form

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,6 +6,7 @@ import iceland from "../assets/picture/Iceland card image.svg"
 import italy from "../assets/picture/Italy card image.svg"
 import dubai from "../assets/picture/Dubai card image.svg"
 import patagonia from "../assets/picture/Patagonia Card Image.svg"
+import travelHero from "../assets/picture/travels background image.svg"
 
 
 const Home = () => {
@@ -104,6 +105,81 @@ const Home = () => {
         </div>
 
     </section>
+
+    <section className="pb-10">
+      
+    <div className="relative w-full min-h-screen overflow-hidden flex items-center justify-center">
+      
+      <div
+        className="absolute top-0 left-0 w-full h-full bg-no-repeat bg-cover bg-center"
+        style={{ backgroundImage: `url(${travelHero})` }}
+      ></div>
+
+      <div className="relative px-4 py-2">
+        <div className="py-10">
+        <h1 className="text-[#8A53FF]  uppercase text-sm font-semibold">Let’s build a community</h1>
+        <h2 style={{ fontFamily: "'Changa One', cursive" }}  className="text-[#434343] text-4xl font-bold py-2">Join our next destination</h2>
+
+
+        </div>
+        
+        <div className="bg-[#FFFFFF] rounded-md w-xl py-6 px-8 text-left">
+          <form action="">
+            <h1 style={{ fontFamily: "'Changa One', cursive" }} className=" text-2xl font-bold">Share your travels</h1>
+            <p className="text-[#576074] py-5">
+              Suggest a new travel destination that you want to see and we will feature in our blog
+            </p>
+
+            <div className="space-y-4">
+            <input type="text" name="name" 
+            className=" block w-lg py-1 px-3 rounded-md text-base border-2 border-gray-300
+             text-gray-900 placeholder:text-gray-500
+             focus:outline-none sm:text-sm/6" placeholder="Destination name"/>
+
+            <select id="country" name="country" autocomplete="country-name"
+             className="block w-lg py-1 px-3 rounded-md text-base border-2 border-gray-300
+              text-gray-500 
+             focus:outline-none sm:text-sm/6" >
+              <option hidden value="">Country of Destination</option>
+              <option>United States</option>
+              <option>Canada</option>
+              <option>Mexico</option>
+            </select>
+            
+            <input type="text" name="username" id="username" 
+            className=" block w-lg py-1 px-3 rounded-md text-base border-2 border-gray-300
+             text-gray-900 placeholder:text-gray-500 
+             focus:outline-none sm:text-sm/6" placeholder="Your Name"/>
+
+     <select id="country" name="country" autocomplete="country-name"
+             className="block w-lg py-1 px-3 rounded-md text-base border-2 border-gray-300
+              text-gray-500 
+             focus:outline-none sm:text-sm/6" >
+              <option hidden value="">Shooting category</option>
+              <option>Fashion</option>
+              <option>Abcd</option>
+              <option>Efgh</option>
+            </select>
+
+            <textarea name="" id="" className=" lock w-lg py-1 px-3 rounded-md text-base border-2
+             border-gray-300 text-gray-900 placeholder:text-gray-500 
+             focus:outline-none sm:text-sm/6"
+             placeholder="Describe what kind of photos you want to focus on"></textarea>
+            </div>
+            <div className="py-2">
+              <button className="text-white bg-[#8A53FF] text-sm rounded-md px-10 py-2">Submit</button>
+            </div>
+           
+
+          </form>
+
+        </div>
+      </div>
+
+      </div>
+
+    </section>
+
 
     </>
   );
